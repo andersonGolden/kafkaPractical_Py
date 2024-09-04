38c86620c380b4f15a4e6cf927b8390de7caca85
andersonGolden <anderson.okwechime@gmail.com>
2024-09-04 15:02:18 +0100

    Delete Main.py

--- Main.py
+++
@@ -1,8 +0,0 @@
-if __name__ == "__main__":
-    kafka_config = {
-        'bootstrap.servers': 'localhost:9092'  # Adjust this to your Kafka broker address
-    }
-    kafka_topic = 'customer_orders'
-
-    service = CustomerOrdersService(kafka_config, kafka_topic)
-    service.create_order("John Doe", "Pizza", 1200.00)
